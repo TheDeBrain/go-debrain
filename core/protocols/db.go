af625ea634626587aaa0888c8994d7fedf56036f
TheDeBrain <thedebrain@gmail.com>
2023-02-05 14:34:52 +0800

    feat : db protocol initialization

---
+++ core/protocols/db.go
@@ -0,0 +1,17 @@
+package protocols
+
+// Protocol for file block head
+type DBTableHead struct {
+	ProtocolType uint8  // Declare the protocol type , 1 byte
+	TableSize    uint64 // The , 8 byte
+}
+
+// Protocol for file block body
+type DBTableBody struct {
+	TableData []byte // file owner data
+}
+
+type DBTable struct {
+	Head DBTableHead // file block head
+	Body DBTableBody // file block body
+}
